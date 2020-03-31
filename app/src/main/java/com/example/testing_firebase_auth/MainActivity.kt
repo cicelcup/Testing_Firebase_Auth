f4bb3953d6a7d4c1d0a0572b22450aed55f922e9
JAPM <cicelcup@gmail.com>
2020-03-31 07:07:08 -0600

    [REF] Add data binding

--- app/src/main/java/com/example/testing_firebase_auth/MainActivity.kt
+++ app/src/main/java/com/example/testing_firebase_auth/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.testing_firebase_auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.testing_firebase_auth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
     }
 }
