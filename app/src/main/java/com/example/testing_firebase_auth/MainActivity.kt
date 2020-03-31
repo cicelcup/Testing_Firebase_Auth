d6ee058ca288c9baa4c7e09d068bb3e6e4dfe985
JAPM <cicelcup@gmail.com>
2020-03-31 07:46:52 -0600

    [NEW] Add firebase auth functionality

--- app/src/main/java/com/example/testing_firebase_auth/MainActivity.kt
+++ app/src/main/java/com/example/testing_firebase_auth/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.testing_firebase_auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.testing_firebase_auth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "Message MainActivity"
+    }
+
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +26,39 @@ class MainActivity : AppCompatActivity() {
             this,
             R.layout.activity_main
         )
+
+        auth = FirebaseAuth.getInstance()
+
+        Log.i(TAG, "User: ${auth.currentUser}")
+
+        with(binding) {
+            signUpButton.setOnClickListener {
+
+            }
+
+            signInButton.setOnClickListener {
+
+            }
+
+            emailValidationButton.setOnClickListener {
+
+            }
+
+            accountValidationButton.setOnClickListener {
+
+            }
+
+            updateAccountButton.setOnClickListener {
+
+            }
+
+            signOutButton.setOnClickListener {
+
+            }
+
+            deleteAccountButton.setOnClickListener {
+
+            }
+        }
     }
 }
