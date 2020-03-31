ddde68794d7c5237f7dcd74ec0ea1c4bf379bf28
JAPM <cicelcup@gmail.com>
2020-03-31 11:23:16 -0600

    [REF] Create generic function to avoid rewrite code in firebase functions

--- app/src/main/java/com/example/testing_firebase_auth/MainActivity.kt
+++ app/src/main/java/com/example/testing_firebase_auth/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.testing_firebase_auth.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 
@@ -34,8 +35,6 @@ class MainActivity : AppCompatActivity() {
         email = "cicelcup@gmail.com"
         password = "123456"
         name = "Augusto"
-
-        Log.i(TAG, "User: ${auth.currentUser?.isEmailVerified}")
 
         with(binding) {
 
@@ -75,68 +74,81 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Sign up with the correspond email and password
     private fun signUp(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    displayLogAndToast("User created")
-                    updateLabel()
-                } else {
-                    displayLogAndToast("Failure user creation ${task.exception}")
-                }
+                taskListener(
+                    task, "User created",
+                    "Failure user creation ${task.exception}"
+                )
             }
     }
 
+    //Sign in with the correspond email and password
     private fun signIn(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    displayLogAndToast("Sign in successful")
-                    updateLabel()
-                } else {
-                    displayLogAndToast("Failure Sign In ${task.exception}")
-                }
+                taskListener(
+                    task, "Sign in successful",
+                    "Failure Sign In ${task.exception}"
+                )
             }
     }
 
+    //Send the email for validation
     private fun sendEmail() {
         auth.currentUser?.sendEmailVerification()
             ?.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    displayLogAndToast("Email validation sent")
-                    updateLabel()
-                } else {
-                    displayLogAndToast("Failure email validation ${task.exception}")
-                }
+                taskListener(
+                    task, "Email validation sent",
+                    "Failure email validation ${task.exception}"
+                )
             }
     }
 
+    //update profile function. It creates a profile update with the correspond information
     private fun updateProfile(name: String) {
         val profileUpdates = UserProfileChangeRequest.Builder()
             .setDisplayName(name)
             .build()
         auth.currentUser?.updateProfile(profileUpdates)
             ?.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    displayLogAndToast("Profile Updated")
-                    updateLabel()
-                } else {
-                    displayLogAndToast("Failure update profile ${task.exception}")
-                }
+                taskListener(
+                    task, "Profile Updated",
+                    "Failure update profile ${task.exception}"
+                )
             }
     }
 
+    //Sign out function
     private fun signOut() {
         auth.signOut()
         displayLogAndToast("User signed out")
         updateLabel()
     }
 
+    //Generic function receiving any kind of Task for displaying and log the result
+    private fun <T : Any?> taskListener(
+        task: Task<T>,
+        successMessage: String,
+        failureMessage: String
+    ) {
+        if (task.isSuccessful) {
+            displayLogAndToast(successMessage)
+            updateLabel()
+        } else {
+            displayLogAndToast(failureMessage)
+        }
+    }
+
+    //Log the message parameter
     private fun displayLogAndToast(message: String) {
         Log.i(TAG, message)
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
+    //Update the label in the UI to show the current information
     private fun updateLabel() {
         binding.information = "User: ${auth.currentUser ?: "Not user"} " +
                 "/ Name: ${auth.currentUser?.displayName ?: "Not name"} " +
